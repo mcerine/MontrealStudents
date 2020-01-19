a6cc24d3c18bd61343f1b9734bd39d4c50d7eab8
Cerine Madi <madi.cerine@gmail.com>
2020-01-19 09:25:39 -0500

    last modifications to dashboard

--- Try1/app/src/main/java/com/example/try1/MainActivity.java
+++ Try1/app/src/main/java/com/example/try1/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.content.Intent;
+import android.content.SharedPreferences;
 
 public class MainActivity extends AppCompatActivity {
 
